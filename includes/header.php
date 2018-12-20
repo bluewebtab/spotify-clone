cfd1fde40fac06800749b880098491774361fb41
Ronny <ronny.icne@gmail.com>
2018-12-20 11:31:39 -0800

    UI design of album page. Connected mysql to webpage to display info.

---
+++ includes/header.php
@@ -0,0 +1,38 @@
+<?php
+include("includes/config.php");
+include("includes/classes/Artist.php");
+include("includes/classes/Album.php");
+include("includes/classes/Song.php");
+
+
+// session_destroy(); Logout
+
+if(isset($_SESSION['userLoggedIn'])){
+  $userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else{
+  header("Location: register.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Welcome to Musik</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+  <div id ="mainContainer">
+      <div id = "topContainer">
+         
+         <?php include("includes/navBarContainer.php"); ?>
+          
+         <div id="mainViewContainer">
+
+                <div id="mainContent">
+
+                
