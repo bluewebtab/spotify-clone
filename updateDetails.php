f9b46e5887a95380ec9b01e53b0146684f366332
Ronny <ronny.icne@gmail.com>
2019-01-09 21:28:55 -0800

    heroku deployment

--- updateDetails.php
+++ updateDetails.php
@@ -8,7 +8,7 @@ include("includes/includedFiles.php");
         <h2>EMAIL</h2>
         <input type="text" class="email" name="email" placeholder="Email address..." value="<?php echo $userLoggedIn->getEmail(); ?>">
         <span class="message"></span>
-        <button class="button" onclick="">SAVE</button>
+        <button class="button" onclick="updateEmail('email')">SAVE</button>
     </div>
 
     <div class="container">
@@ -18,7 +18,7 @@ include("includes/includedFiles.php");
         <input type="password" class="newPassword2" name="newPassword2" placeholder="Confirm password">
 
         <span class="message"></span>
-        <button class="button" onclick="">SAVE</button>
+        <button class="button" onclick="updatePassword('oldPassword', 'newPassword1', 'newPassword2')">SAVE</button>
   
     </div>
 </div>
