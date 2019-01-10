c4e337e35cff2ab64594d51e3046407e78473194
Ronny <ronny.icne@gmail.com>
2019-01-10 00:26:26 -0800

    Deletion

--- includes/config.php
+++ includes/config.php
@@ -5,11 +5,7 @@
   $timezone = date_default_timezone_set("America/Los_Angeles");
 
   $con = mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "b881f0a57f7cb9", "bf2c1713", "heroku_4ffe31d912bbbd6");
-  if($con){
-    echo "It works";
-  }else{
-    echo "It doesn't";
-  }
+  
   // $con = mysqli_connect("localhost", "ronny", "hello123", "musik");
 
   if(mysqli_connect_errno()){
