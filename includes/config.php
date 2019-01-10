9e6ea06fcf3f69c16939678261d3813175bac574
Ronny <ronny.icne@gmail.com>
2019-01-10 00:00:58 -0800

    Changed connection for heroku

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,9 @@
 
   $timezone = date_default_timezone_set("America/Los_Angeles");
 
-  $con = mysqli_connect("localhost", "ronny", "hello123", "musik");
+  $con = mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "b881f0a57f7cb9", "bf2c1713", "heroku_4ffe31d912bbbd6");
+
+  // $con = mysqli_connect("localhost", "ronny", "hello123", "musik");
 
   if(mysqli_connect_errno()){
     echo "Failed to connect: " . mysqli_connect_errno();
